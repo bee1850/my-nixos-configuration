5d6d46d755f4c376f07777e0d238397f6e4ae4b2
Berkan Erkasap <bee1850@thi.de>
2023-12-10 15:43:37 +0100

    Remove unnecessary container configurations and update networking settings

--- machines/wsl/configuration.nix
+++ machines/wsl/configuration.nix
@@ -19,55 +19,6 @@
     secret-key-files = /root/cache-priv-key.pem
   '';
 
-  containers.database =
-    {
-      restartIfChanged = true;
-      config =
-        { config, pkgs, ... }:
-        {
-          system.stateVersion = "23.05";
-          environment.shellAliases = {
-            psql-login = "sudo -u postgres psql postgres";
-          };
-
-          nixpkgs.config.allowUnfree = true;
-
-          services.mongodb = {
-            enable = true;
-            enableAuth = true;
-            bind_ip = "0.0.0.0";
-            initialRootPassword = "root";
-          };
-
-          services.postgresql.enable = true;
-          services.pgadmin.enable = true;
-          services.pgadmin.initialEmail = "bee1850@thi.de";
-          services.pgadmin.initialPasswordFile = pkgs.writeText "pgadminPW" ''
-            adminadmin
-          '';
-          #services.prometheus.exporters.postgres.enable = true;
-          #services.prometheus.exporters.postgres.port = 9003;
-        };
-        forwardPorts = [
-        {
-          containerPort = 5432;
-          hostPort = 5432;
-          protocol = "tcp";
-        }
-        {
-          containerPort = 5050;
-          hostPort = 5050;
-          protocol = "tcp";
-        }
-        {
-          containerPort = 27017;
-          hostPort = 27017;
-          protocol = "tcp";
-        }
-      ];
-    };
-
-   networking.firewall.allowedTCPPorts = [ 5432 5050 27017 ];
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
