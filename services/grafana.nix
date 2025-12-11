da25a1a6405b2810f781b664145ab37d4fa90564
Berkan E. <bee1850@thi.de>
2025-12-11 17:04:44 +0100

    fix: update Grafana proxyPass to use the correct HTTP port

--- services/grafana.nix
+++ services/grafana.nix
@@ -41,7 +41,7 @@
     enable = true;
     virtualHosts."192.168.0.107" = {
       locations."/" = {
-        proxyPass = "http://127.0.0.1:${toString config.services.grafana.port}";
+        proxyPass = "http://127.0.0.1:${toString config.services.grafana.settings.server.http_port}";
         recommendedProxySettings = true;
         proxyWebsockets = true;
         extraConfig = ''
